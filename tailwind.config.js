38c39e1790326a4fa12831832584c52dfa570f21
ThatguyscottieC <officialscottiechambers@gmail.com>
2023-03-22 15:12:09 -0400

    UI change

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,14 @@ module.exports = {
       },
     ],
   },
-  extend: {},
+  extend: {
+    screens: {
+      sm: { max: "639px" },
+      md: { max: "767px" },
+      lg: { max: "1023px" },
+      xl: { max: "1279px" },
+    },
+  },
 
   plugins: [require("daisyui")],
 };
